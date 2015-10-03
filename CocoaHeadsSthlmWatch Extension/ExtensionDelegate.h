a0e87a7d9a510ac8e79f6a7d91b5cd75fbdeba03
Pedro Mancheno <pedromancheno@gmail.com>
2015-10-03 13:18:30 +0200

    Add Watch Target

---
+++ CocoaHeadsSthlmWatch Extension/ExtensionDelegate.h
@@ -0,0 +1,13 @@
+//
+//  ExtensionDelegate.h
+//  CocoaHeadsSthlmWatch Extension
+//
+//  Created by Pedro Mancheno on 03/10/15.
+//  Copyright © 2015 Lifesum. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+@interface ExtensionDelegate : NSObject <WKExtensionDelegate>
+
+@end
---
+++ CocoaHeadsSthlmWatch Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  CocoaHeadsSthlmWatch Extension
+//
+//  Created by Pedro Mancheno on 03/10/15.
+//  Copyright © 2015 Lifesum. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
