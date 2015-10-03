2cf9974dcbb651f24daf3d414a27829f90e69095
Pedro Mancheno <pedromancheno@gmail.com>
2015-10-03 13:17:13 +0200

    Initial Commit

---
+++ CocoaHeadsSthlm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CocoaHeadsSthlm
+//
+//  Created by Pedro Mancheno on 03/10/15.
+//  Copyright © 2015 Lifesum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CocoaHeadsSthlm/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CocoaHeadsSthlm
+//
+//  Created by Pedro Mancheno on 03/10/15.
+//  Copyright © 2015 Lifesum. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
